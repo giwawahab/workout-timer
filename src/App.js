f9991ca15306cf58f51c7daaf232b5a0dfffb10a
giwawahab <giwawahabtomiwa2022@gmail.com>
2026-05-10 12:47:17 +0100

    optimize timer with memo and useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,36 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Calculator from "./Calculator";
 import ToggleSounds from "./ToggleSounds";
 
-function App() {
-  const [allowSound, setAllowSound] = useState(true);
-  const [time, setTime] = useState(formatTime(new Date()));
-
-  // Will be be AM or PM
-  const partOfDay = time.slice(-2);
-
-  const workouts = [
-    {
-      name: "Full-body workout",
-      numExercises: partOfDay === "AM" ? 9 : 8,
-    },
-    {
-      name: "Arms + Legs",
-      numExercises: 6,
-    },
-    {
-      name: "Arms only",
-      numExercises: 3,
-    },
-    {
-      name: "Legs only",
-      numExercises: 4,
-    },
-    {
-      name: "Core only",
-      numExercises: partOfDay === "AM" ? 5 : 4,
-    },
-  ];
 
   function formatTime(date) {
     return new Intl.DateTimeFormat("en", {
@@ -41,6 +12,43 @@ function App() {
       second: "2-digit",
     }).format(date);
   }
+
+
+function App() {
+  const [allowSound, setAllowSound] = useState(true);
+  const [time, setTime] = useState(formatTime(new Date()));
+
+  // Will be be AM or PM
+  const partOfDay = time.slice(-2);
+
+  const workouts = useMemo(
+    function () {
+      return [
+        {
+          name: "Full-body workout",
+          numExercises: partOfDay === "AM" ? 9 : 8,
+        },
+        {
+          name: "Arms + Legs",
+          numExercises: 6,
+        },
+        {
+          name: "Arms only",
+          numExercises: 3,
+        },
+        {
+          name: "Legs only",
+          numExercises: 4,
+        },
+        {
+          name: "Core only",
+          numExercises: partOfDay === "AM" ? 5 : 4,
+        },
+      ];
+    },
+    [partOfDay],
+  );
+
 
   useEffect(function () {
     const id = setInterval(function () {
